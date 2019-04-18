b04d2a6631f2cb1991a947c496dc4ce3a840c3f8
ianyshuang <ianyshuang@gmail.com>
2019-04-19 00:13:32 +0800

    Change sort route to dynamic params and query

--- routes/sort.js
+++ routes/sort.js
@@ -3,36 +3,13 @@ const router = express.Router()
 const Restaurant = require('../models/restaurant')
 const category = require('./category') // 載入 category.exec()回傳的Promise
 
-function getOrder(order) {
-  return (order === 'true') ? 'asc' : 'desc'
-}
 
-router.get('/name', (req, res) => {
-  // 設定是'asc' / 'desc'
-  const order = getOrder(req.query.asc)
-  Restaurant.find().sort({ name: order }).exec((err, restaurants) => {
-    if (err) return console.error(err)
+router.get('/:condition', (req, res) => {
+  const condition = req.params.condition
+  const order = req.query.order
 
-    category.then(result => {
-      return res.render('index', { restaurants: restaurants, categories: result })
-    })
-  })
-})
-
-router.get('/name_en', (req, res) => {
-  const order = getOrder(req.query.asc)
-  Restaurant.find().sort({ name_en: order }).exec((err, restaurants) => {
-    if (err) return console.error(err)
-
-    category.then(result => {
-      return res.render('index', { restaurants: restaurants, categories: result })
-    })
-  })
-})
-
-router.get('/rating', (req, res) => {
-  const order = getOrder(req.query.asc)
-  Restaurant.find().sort({ rating: order }).exec((err, restaurants) => {
+  // ES6語法，在變數外加中括號可
+  Restaurant.find().sort({ [condition]: order }).exec((err, restaurants) => {
     if (err) return console.error(err)
 
     category.then(result => {
